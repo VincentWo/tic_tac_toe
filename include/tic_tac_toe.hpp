84de9ea0fc5b2ec86181684134355d74ff82204e
Vincent Woltmann <v.woltmann@web.de>
2016-11-10 00:57:11 +0100

    Implemented the field class
    The field class was implemented and the interface was slightly
    changed(The fill method was renamed to occupy). This change also
    introduces a change in the interface requirements for the Grid class.
    Now the Grid class has to be constructible like this:
    
        Grid{width, height, field}
    
    where width and height are of type Grid::size_type and field of type
    Grid::value_type

--- include/tic_tac_toe.hpp
+++ include/tic_tac_toe.hpp
@@ -1,5 +1,6 @@
-#include "grid.hpp"
+#include <exception>
 
+#include "grid.hpp"
 namespace tic_tac_toe
 {
 	enum class player
@@ -9,25 +10,83 @@ namespace tic_tac_toe
 		none,
 		unknown
 	};
+
+	class basic_game_base
+	{
+		public:
+			basic_game_base() : current_player_{player::one}
+			{}
+			player current_player() noexcept
+			{
+				return current_player_;
+			}
+			void next_player() noexcept
+			{
+				if(current_player_ == player::one)
+				{
+					current_player_ = player::two;
+				}
+				else
+				{
+					current_player_ = player::one;
+				}
+			}
+		private:
+			player current_player_;
+	};
+
 	class field
 	{
 		public:
-			bool empty() const;
-			void fill();
+			explicit field(basic_game_base& game_base) : game_base{game_base}
+			{}
+
+			bool empty() const noexcept
+			{
+				return owner() == player::none;
+			}
+			player owner() const noexcept
+			{
+				return owner_;
+			}
+			void occupy()
+			{
+				if(!empty())
+				{
+					throw already_occupied{};
+				}
+				owner_ = game_base.current_player();
+				game_base.next_player();
+			}
+
+			class already_occupied : public std::exception
+			{
+				public:
+					const char* what() const noexcept override 
+					{
+						return "Tried to occupy an occupied field!";
+					}
+			};
+		private:
+			player owner_ = player::none;
+			basic_game_base& game_base;
 	};
 
+
 	template<typename Grid>
-	class basic_game
+	class basic_game : public basic_game_base
 	{
 		public:
 			using size_type = typename Grid::size_type;
 
 			basic_game() : basic_game{3,3}
 			{}
-			basic_game(size_type width, size_type heigth) : grid{width,heigth}
+			basic_game(size_type width, size_type heigth)
+				:	basic_game_base{},
+					grid{width,heigth, field{*this}}
 			{}
 
-			player winner();
+			player winner() noexcept;
 
 
 			field& operator()(size_type x, size_type y)
@@ -38,7 +97,6 @@ namespace tic_tac_toe
 			{
 				return grid(x, y);
 			}
-
 		private:
 			Grid grid;
 	};
